d075954026b8638803647e91a77c7dba112253e2
zgruza <47500410+zgruza@users.noreply.github.com>
2019-06-16 01:13:29 +0200

    Update get_tor.sh

--- get_tor.sh
+++ get_tor.sh
@@ -2,7 +2,6 @@
 i=0 # Start number {eg. i=0; 0->save link, 1->save link} || {eg2. i=3; 3->save link, 4->savev link, etc..}
 while [ -d /var/lib/tor/hidden_service$i ];
 do
-   mkdir "/var/www/${i}"
   workjob=$(cat /var/lib/tor/hidden_service$i/hostname)
   echo -e "${workjob}" >> "/var/www/links.txt"
   i=$((i+1))
