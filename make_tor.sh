4f1ef83e723db2560156866f6823812f5155b967
zgruza <47500410+zgruza@users.noreply.github.com>
2019-06-16 00:54:48 +0200

    Update make_tor.sh

--- make_tor.sh
+++ make_tor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for((i=0;i<=4;i++))
+for((i=0;i<=4;i++)) # == 5 new links
   mkdir "\var\lib\tor\hidden_service${i}"
   echo -e "HiddenServiceDir /var/lib/tor/hidden_service${i}\n" >> "\etc\tor\torrc"
   echo -e "HiddenServicePort 80 127.0.0.1:80\n" >> "\etc\tor\torrc"
