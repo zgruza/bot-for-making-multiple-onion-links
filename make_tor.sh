6937c62fc6967f3f2e1519c864d0311005c61381
zgruza <47500410+zgruza@users.noreply.github.com>
2019-06-16 01:10:46 +0200

    Rewrite whole script

--- make_tor.sh
+++ make_tor.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
 for((i=0;i<=4;i++)) # == 5 new links
-  mkdir "\var\lib\tor\hidden_service${i}"
-  echo -e "HiddenServiceDir /var/lib/tor/hidden_service${i}\n" >> "\etc\tor\torrc"
-  echo -e "HiddenServicePort 80 127.0.0.1:80\n" >> "\etc\tor\torrc"
+do
+  echo -e "HiddenServiceDir /var/lib/tor/hidden_service${i}" >> "/etc/tor/torrc"
+  echo -e "HiddenServicePort 80 127.0.0.1:80\n" >> "/etc/tor/torrc"
 done
+chmod 700 -R /var/lib/tor
+chmod 700 -R /var/lib/tor/*
+service tor reload
+echo "Done"
