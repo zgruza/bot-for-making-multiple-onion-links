81506de4f9a81a6d4225b409a71b05f395abc876
IGuessNobody <horacek.pavel@protonmail.com>
2019-12-23 22:12:27 +0100

    Add a feature to input through argument
    Changing the number of addresses through writing to code isn't really a good solution.
    So here is a way to input it through command argument and even memorize it to the get_tor.sh

--- get_tor.sh
+++ get_tor.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-i=0 # Start number {eg. i=0; 0->save link, 1->save link} || {eg2. i=3; 3->save link, 4->savev link, etc..}
+
+buff_dir='./'
+
+num_test='^[0-9]+$'
+if [ $# -eq 0 ]
+then
+	i=$(cat ${buff_dir}prev_arg)
+else
+	if ! [[ $1 =~ $num_test ]];
+	then
+		echo "Argument not a number!" >&2
+		exit
+	fi
+	i=$1
+fi
+
 while [ -d /var/lib/tor/hidden_service$i ];
 do
   workjob=$(cat /var/lib/tor/hidden_service$i/hostname)
--- make_tor.sh
+++ make_tor.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-for((i=0;i<=4;i++)) # == 5 new links
+
+buff_dir='./'
+
+num_test='^[0-9]+$'
+if ! [[ $1 =~ $num_test ]];
+then
+    echo "Argument not a number!" >&2
+    exit
+fi
+
+for((i=0;i<$1;i++)) # == 5 new links
 do
   echo -e "HiddenServiceDir /var/lib/tor/hidden_service${i}" >> "/etc/tor/torrc"
   echo -e "HiddenServicePort 80 127.0.0.1:80\n" >> "/etc/tor/torrc"
@@ -8,3 +18,5 @@ chmod 700 -R /var/lib/tor
 chmod 700 -R /var/lib/tor/*
 service tor reload
 echo "Done"
+
+echo "$1" > ${buff_dir}prev_arg
